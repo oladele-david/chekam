85306da5359dff3929af6ff113299fc7391513f0
adelajahalima <adelajahalima@gmail.com>
2024-10-05 16:57:22 +0100

    Add the frontend folder, did initial installations and create stating files.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='text-center'>
+        <h1 className='text-yellow-300 text-center'>Hello World</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
