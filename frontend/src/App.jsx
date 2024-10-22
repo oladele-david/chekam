77ccb238f49c78cf7ecd586e14ff024d903bfd88
oladele.david <tdboy59@gmail.com>
2024-10-22 13:42:33 +0100

    🔧 fix: ensure correct JavaScript MIME type in nginx.conf
    - Included mime.types in nginx.conf to serve JavaScript files with the correct MIME type.
    - Resolved MIME type errors for module scripts.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/request-email" element={<RequestEmail />} />
